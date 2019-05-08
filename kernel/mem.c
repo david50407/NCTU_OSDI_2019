a0b906d5cad09d1b96a552556bde192a68d2fe59
David Kuo <s50407s@gmail.com>
2019-05-08 14:58:28 +0800

    [Lab 6] mem_init_mp

--- kernel/mem.c
+++ kernel/mem.c
@@ -253,7 +253,14 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	// TODO:
 	// Lab6: Your code here:
+	
+	int i = 0;
+	void *kstacktop_i = NULL;
+	for (; i < NCPU; ++i) {
+		kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
 
+		boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, ROUNDUP(KSTKSIZE, PGSIZE), PADDR(percpu_kstacks[i]), PTE_W | PTE_P);
+	}
 }
 
 // --------------------------------------------------------------
